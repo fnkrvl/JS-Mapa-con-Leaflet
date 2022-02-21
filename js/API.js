59a83a5c6cf9d1ee0cb7c0533da601c0da25c2e7
Franco <franco.rvl91@gmail.com>
2022-02-21 09:02:07 -0300

    Reference Comment

--- js/API.js
+++ js/API.js
@@ -8,7 +8,7 @@ class UI {
 
     inicializarMapa() {
          // Inicializar y obtener la propiedad del mapa
-         const map = L.map('mapa').setView([-29.1567, -59.6685], 11);
+         const map = L.map('mapa').setView([-29.1567, -59.6685], 11); // [X,Y],Zoom
          const enlaceMapa = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
          L.tileLayer(
              'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
